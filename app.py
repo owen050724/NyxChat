3ea7213c964940b359ce940fda6e33570aefe2b0
Owen <parkyeonoh05@gmail.com>
2024-11-13 10:32:53 +0900

    dev-ops test

--- app.py
+++ app.py
@@ -2,98 +2,47 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room, leave_room, emit
 import random
 import string
-import threading
-import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-rooms = {}
-waiting_timers = {}
-
+rooms = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        code = request.form['code']
-        return redirect(url_for('waiting', code=code))
+        nickname = request.form['nickname']
+        return redirect(url_for('chat', nickname=nickname))
     return render_template('index.html')
 
+@app.route('/chat/<nickname>')
+def chat(nickname):
+    return render_template('chat.html', nickname=nickname)
 
-@app.route('/waiting/<code>')
-def waiting(code):
-    return render_template('waiting.html', code=code)
-
-
-@app.route('/chat/<room>/<nickname>')
-def chat(room, nickname):
-    return render_template('chat.html', room=room, nickname=nickname)
-
-
-@socketio.on('join_waiting')
-def on_join_waiting(data):
-    code = data['code']
-    sid = request.sid
-    nickname = random_nickname()
-
-    if code in rooms:
-        rooms[code].append(sid)
-        if len(rooms[code]) == 2:
-            emit('matched', {'nickname': nickname}, room=sid)
-            emit('matched', {'nickname': random_nickname()}, room=rooms[code][0])
-            # Cancel timer if a match is found before timeout
-            if code in waiting_timers:
-                waiting_timers[code].cancel()
-                del waiting_timers[code]
-    else:
-        rooms[code] = [sid]
-        timer = threading.Timer(60.0, timeout, args=[code, sid])
-        waiting_timers[code] = timer
-        timer.start()
-
-
-@socketio.on('nickname_set')
-def nickname_set(data):
-    room = data['room']
+@socketio.on('join_chat')
+def on_join_chat(data):
     nickname = data['nickname']
-    join_room(room)
-    emit('chat_message', {'message': f'{nickname} has joined the chat.'}, room=room)
-
+    sid = request.sid
+    rooms.append(sid)
+    join_room('chatroom')
+    emit('chat_message', {'message': f'{nickname} has joined the chat.'}, room='chatroom')
 
 @socketio.on('send_message')
 def handle_send_message(data):
-    room = data['room']
     message = data['message']
     nickname = data['nickname']
-    emit('chat_message', {'message': f'{nickname}: {message}'}, room=room)
-
+    emit('chat_message', {'message': f'{nickname}: {message}'}, room='chatroom')
 
 @socketio.on('exit_chat')
 def exit_chat(data):
-    room = data['room']
-    leave_room(room)
-    emit('chat_message', {'message': 'The chat has ended.'}, room=room)
-    del rooms[room]
-    if room in waiting_timers:
-        del waiting_timers[room]
-
+    nickname = data['nickname']
+    leave_room('chatroom')
+    emit('chat_message', {'message': f'{nickname} has left the chat.'}, room='chatroom')
 
 # Helper function to create a random nickname
 def random_nickname():
     return 'User' + ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
-
-
-# Timeout function for waiting room
-def timeout(code, sid):
-    if code in rooms and sid in rooms[code]:
-        rooms[code].remove(sid)
-        socketio.emit('timeout', room=sid)
-        if not rooms[code]:
-            del rooms[code]
-        if code in waiting_timers:
-            del waiting_timers[code]
-
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,7 @@
 const socket = io();
 
-function joinWaiting(code) {
-    socket.emit('join_waiting', { code });
-
-    let timer = 60;
-    const timerElement = document.getElementById('timer');
-    const countdown = setInterval(() => {
-        timer--;
-        timerElement.textContent = timer;
-        if (timer <= 0) {
-            clearInterval(countdown);
-            alert('No participants');
-            window.location.href = '/';
-        }
-    }, 1000);
-
-    socket.on('matched', (data) => {
-        clearInterval(countdown);
-        const nickname = prompt('Enter your nickname:', data.nickname);
-        window.location.href = `/chat/${code}/${nickname}`;
-    });
-
-    socket.on('timeout', () => {
-        clearInterval(countdown);
-        alert('No participants');
-        window.location.href = '/';
-    });
-}
-
-function setupChat(room, nickname) {
-    socket.emit('nickname_set', { room, nickname });
+function setupChat(nickname) {
+    socket.emit('join_chat', { nickname });
 
     socket.on('chat_message', (data) => {
         const chatWindow = document.getElementById('chat-window');
@@ -44,13 +16,13 @@ function sendMessage() {
     const messageInput = document.getElementById('message-input');
     const message = messageInput.value;
     if (message.trim() !== '') {
-        socket.emit('send_message', { room, message, nickname });
+        socket.emit('send_message', { message, nickname });
         messageInput.value = '';
     }
 }
 
 function exitChat() {
-    socket.emit('exit_chat', { room });
+    socket.emit('exit_chat', { nickname });
     alert('The chat has ended.');
     window.location.href = '/';
 }
